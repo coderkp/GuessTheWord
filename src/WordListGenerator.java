9795b7c7820db303d93e734b1bd1ef354aad5f77
Kush Parikh <kushparikh01@gmail.com>
2018-08-09 16:01:06 +0530

    Functionality to generate ArrayList of required length

--- src/WordListGenerator.java
+++ src/WordListGenerator.java
@@ -1,3 +1,32 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordListGenerator {
 
+    public List<String> getWordList(int wordLength)
+    {   ArrayList<String> words = new ArrayList<>();
+        File file = new File("./src/sowpods.txt");
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(file));
+            String st;
+            while ((st = br.readLine()) != null) {
+               if(st.length()!=wordLength)
+                   continue;
+               words.add(st);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return words;
+    }
+    public static void main(String args[])
+    {
+        WordListGenerator wlg = new WordListGenerator();
+        System.out.println(wlg.getWordList(4));
+    }
 }
